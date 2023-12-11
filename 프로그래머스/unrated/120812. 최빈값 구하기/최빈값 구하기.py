a951273a596cb9ccd7f646c5315d08a51b309965
ChaeEun Han <68267916+chaeeun-Han@users.noreply.github.com>
2023-12-11 20:21:44 +0900

    Update 최빈값 구하기.py

--- 프로그래머스/unrated/120812. 최빈값 구하기/최빈값 구하기.py
+++ 프로그래머스/unrated/120812. 최빈값 구하기/최빈값 구하기.py
@@ -1,5 +1,4 @@
 def solution(array):
-    maxCnt,answer = 0, 0
     data = dict()
     
     for i in array:
@@ -8,7 +7,5 @@ def solution(array):
         else:
             data[i] = array.count(i)
             
-    tmp = [k for k,v in data.items() if max(data.values()) == v]
-    answer = -1 if len(tmp) > 1 else tmp[0]
-    
-    return answer
+    tmp = [k for k,v in data.items() if max(data.values()) == v]    
+    return -1 if len(tmp) > 1 else tmp[0]
