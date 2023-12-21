4d24dfa4cef4bdd13bc854d000f275113baadbfb
ChaeEun Han <68267916+chaeeun-Han@users.noreply.github.com>
2023-12-21 12:13:32 +0900

    [Silver IV] Title: 알바생 강호, Time: 100 ms, Memory: 34972 KB -BaekjoonHub

--- 백준/Silver/1758. 알바생 강호/알바생 강호.py
+++ 백준/Silver/1758. 알바생 강호/알바생 강호.py
@@ -1,15 +1,16 @@
-n = int(input())
-tip = list()
-total = 0
+import sys
+input = sys.stdin.readline
 
-for i in range(n):
-    tip.append(int(input()))
+n = int(input().rstrip())
+tip = [int(input().rstrip()) for _ in range(n)]
+total = 0
 
 tip.sort(reverse=True)
 
 for i in range(1, len(tip) + 1):
     tmp = tip[i-1] - (i - 1)
-    if tmp > 0:
-        total += tmp
+    if tmp < 0:
+        continue
+    total += tmp
 
 print(total)
