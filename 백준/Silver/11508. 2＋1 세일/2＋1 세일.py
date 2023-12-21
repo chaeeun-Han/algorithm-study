aa34aa7bdec733711c1204b1644557e5353479a1
ChaeEun Han <68267916+chaeeun-Han@users.noreply.github.com>
2023-12-21 13:06:41 +0900

    Update 2＋1 세일.py

--- 백준/Silver/11508. 2＋1 세일/2＋1 세일.py
+++ 백준/Silver/11508. 2＋1 세일/2＋1 세일.py
@@ -17,3 +17,18 @@ for i in range(len(price) - (len(price) % 3)):
     cnt += 1
 
 print(total)
+
+# 조금 더 나은 풀이
+# 내림차순으로 정렬 후, total에서 3의 배수에 속하는 것들을 빼주면 된다.
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+price = [int(input().rstrip()) for _ in range(n)]
+price.sort(reverse=True)
+
+total = 0
+for i in range(2,len(price),3):
+  total += price[i]
+
+print(sum(price) - total)
