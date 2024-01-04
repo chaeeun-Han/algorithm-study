dee1868ed23fbfd185e1e4e2c08eecb6018e39f3
ChaeEun Han <68267916+chaeeun-Han@users.noreply.github.com>
2024-01-04 19:58:08 +0900

    [Bronze II] Title: 커트라인, Time: 172 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.py
@@ -0,0 +1,12 @@
+n, k = map(int, input().split())
+array = list(map(int, input().split()))
+
+# 선택 정렬
+for i in range(1, len(array)):
+    for j in range(i, 0, -1):
+        if array[j] < array[j-1]:
+            array[j], array[j-1] = array[j-1], array[j]
+        else:
+            break
+
+print(array[-k])
