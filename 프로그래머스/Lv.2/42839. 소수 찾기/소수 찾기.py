d7b0c26fc4a69be71eb232d5cb4a6a97778f6547
ChaeEun Han <68267916+chaeeun-Han@users.noreply.github.com>
2024-01-12 11:00:18 +0900

    [level Lv.2] Title: 소수 찾기, Time: 0.60 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.2/42839. 소수 찾기/소수 찾기.py
@@ -0,0 +1,21 @@
+import itertools
+def solution(numbers):
+    number = [i for i in numbers]
+    num = []
+
+    # 숫자 조합
+    for i in range(len(number)):
+        for j in itertools.permutations(number, i+1):
+            num.append(int(''.join(j)))
+    num_list = list(set(num))
+    if 1 in num_list:num_list.remove(1)
+    if 0 in num_list:num_list.remove(0)
+    answer = len(num_list)
+
+    for i in num_list:
+        for j in range(2, i):  # 2부터 x-1까지의 모든 숫자
+            if i % j == 0:
+                answer -= 1
+                break
+
+    return answer
