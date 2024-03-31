4ab9e8382d406167d2eda36bda3a50959cc1a8fe
ChaeEun Han <68267916+chaeeun-Han@users.noreply.github.com>
2024-03-31 22:11:54 +0900

    [Silver II] Title: 연속합, Time: 96 ms, Memory: 38828 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -1,15 +1,14 @@
-# 28min 23sec
 import sys
 input = sys.stdin.readline
 
 n = int(input())
 nums = list(map(int, input().split()))
 
-current_sum = nums[0]
-max_sum = nums[0]
+# dp[i]는 nums[i]를 포함한 최대 연속 부분합을 저장합니다.
+dp = [0] * n
+dp[0] = nums[0]
 
 for i in range(1, n):
-    current_sum = max(nums[i], current_sum + nums[i])
-    max_sum = max(max_sum, current_sum)
+    dp[i] = max(nums[i], dp[i-1] + nums[i])
 
-print(max_sum)
+print(max(dp))
