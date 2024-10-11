85474b659532ec460570cad1fd736b9c40f08e64
ChaeEun Han <68267916+chaeeun-Han@users.noreply.github.com>
2024-10-11 14:33:56 +0900

    [Bronze V] Title: Hello World, Time: 88 ms, Memory: 13928 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -0,0 +1,9 @@
+public class Main {
+    public void solution() {
+        System.out.println("Hello World!");
+    }
+    
+    public static void main(String[] args) {
+        new Main().solution();
+    }
+}
