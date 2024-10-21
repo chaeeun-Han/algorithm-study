31329de26d0e7ddbe874eafe00a16353151ed3c3
ChaeEun Han <68267916+chaeeun-Han@users.noreply.github.com>
2024-10-21 16:13:31 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -1,15 +1,24 @@
 def solution(s):
+#     answer = ''
+#     buf = [word.lower() for word in s.split()]
+    
+#     for word in buf:
+#         answer += ' ' + word.capitalize()
+        
+#     return answer[1:]
+    isFirst = True
     answer = ''
     s = s.lower()
-    flag = True
-
-    for i in s:
-        if i == ' ':
-            flag = True
-        elif flag:
-            i = i.upper()
-            flag = False
-
-        answer += i
-
+    
+    for word in s:
+        if (isFirst and word != ' '):
+            answer += word.upper()
+            isFirst = False
+        elif (word == ' '):
+            answer += word
+            isFirst = True
+        else:
+            answer += word
+            
     return answer
+            
